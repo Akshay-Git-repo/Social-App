42d84deee2054ba3b7af2a694627519ced1402b0
Akshay-Git-repo <67847856+Akshay-Git-repo@users.noreply.github.com>
2020-08-29 02:40:40 +0530

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -11,36 +11,6 @@ const accessLogStream= rfs.createStream('access.log',{
 
 });
 
-const development={
-
-    name:"development",
-    asset_path:"./assets",
-    session_cookie_key:'balhsomething',
-    db:'codeial_development',
-    smtp:{
-        service:'gmail',
-        host: "smtp.gmail.com",
-        port: 587, //port for TLS ,foer more details check gmail smtp settings
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: 'akshaykharade95@gmail.com', // generated ethereal user
-          pass: 'kharade6041995@', // generated ethereal password
-        }
-      },
-
-    google_client_id: '572636840808-9dsoevg22c20i74a62h9g6j7etokiv5e.apps.googleusercontent.com',
-    google_clientSecret: 'TZHygtekkk6_MMaAE4qhC_L8',
-    google_callbackURL: "http://localhost:8000/users/auth/google/callback",
-    jwt_secret_or_key:'codeial',
-    morgan:{
-
-      mode:'dev',
-      options:{stream:accessLogStream}
-    }
-
-};
-
-
 const production={
 
     name:"production",
